cfba7d15379d1a32de09b080a1bbeae5d8d64187
aimee_t <aimeetemple14@gmail.com>
2024-04-29 14:31:18 -0400

    Initial commit

---
+++ Animations3/Animations3App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations3App.swift
+//  Animations3
+//
+//  Created by Temple on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Animations3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
